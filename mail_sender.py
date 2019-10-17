04e3afc5c38748ddcb3828ec18069ef122b050ef
joepol <cpumaster15@gmail.com>
2019-10-18 00:14:46 +0300

    Simple mail sending text message to a list of receipents loaded from a JSON file and a dev accout - email account with the single purpose of sending status messages

---
+++ mail_sender.py
@@ -0,0 +1,26 @@
+import smtplib
+import ssl
+import json
+
+port = 465  # most be this port for SSL
+smtp_server = "smtp.gmail.com"
+
+with open('data.txt', 'r') as json_file:
+    data = json.load(json_file)
+    sender_mail = data['sender_mail']
+    sender_password = data['sender_password']
+    receipents = data['receipents'] # a list of email addresses
+
+message = """\
+Subject: Timelapse GIF
+
+This message contain an animated GIF of last 24H time lapse."""
+
+print("sender_mail is {}".format(sender_mail))
+print(type(receipents))
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(host=smtp_server, port=port, context=context) as server:
+    server.login(sender_mail, sender_password)
+    for receipent_mail in receipents:
+        server.sendmail(sender_mail, receipent_mail, message)
