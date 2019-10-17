056d6860eb5038164ec1382617557cac5631e239
joepol <cpumaster15@gmail.com>
2019-10-18 00:16:36 +0300

    minor name refactor

--- mail_sender.py
+++ mail_sender.py
@@ -9,7 +9,7 @@ with open('data.txt', 'r') as json_file:
     data = json.load(json_file)
     sender_mail = data['sender_mail']
     sender_password = data['sender_password']
-    receipents = data['receipents'] # a list of email addresses
+    receipents_mails = data['receipents'] # a list of email addresses
 
 message = """\
 Subject: Timelapse GIF
@@ -17,10 +17,9 @@ Subject: Timelapse GIF
 This message contain an animated GIF of last 24H time lapse."""
 
 print("sender_mail is {}".format(sender_mail))
-print(type(receipents))
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(host=smtp_server, port=port, context=context) as server:
     server.login(sender_mail, sender_password)
-    for receipent_mail in receipents:
+    for receipent_mail in receipents_mails:
         server.sendmail(sender_mail, receipent_mail, message)
