cd0b78de3778f268cfcb1c899c7041ccab82be94
joepol <cpumaster15@gmail.com>
2019-10-20 01:30:05 +0300

    capturing images in 1 sec intervals with epoch time stamp for later sorting using openCV lib (cv2)

---
+++ timelapse_capture.py
@@ -0,0 +1,21 @@
+import cv2
+import time
+
+camera_port = 0
+camera = cv2.VideoCapture(camera_port, cv2.CAP_DSHOW)
+if not camera.isOpened():
+    raise Exception("Could not open video device")
+
+# camera.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
+# camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
+
+for i in range(5):
+    print("Capturing an image")
+    return_value, image = camera.read()
+    image_file_name = "{}.jpg".format(int(time.time()))
+    cv2.imwrite(image_file_name, image)
+    time.sleep(1)
+    print("Image captured and saved by the name {}".format(image_file_name))
+
+camera.release()
+cv2.destroyAllWindows()
