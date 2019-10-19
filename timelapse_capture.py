4ba20094f160896005ee222b015a4fa10f2d0008
joepol <cpumaster15@gmail.com>
2019-10-20 02:48:04 +0300

    capturing images to a specified folder

--- timelapse_capture.py
+++ timelapse_capture.py
@@ -8,11 +8,13 @@ if not camera.isOpened():
 
 # camera.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
 # camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
+# TODO : create a directory for each day (or any sampling period)
+path = 'images\\'
 
 for i in range(5):
     print("Capturing an image")
     return_value, image = camera.read()
-    image_file_name = "{}.jpg".format(int(time.time()))
+    image_file_name = path + "{}.png".format(int(time.time()))
     cv2.imwrite(image_file_name, image)
     time.sleep(1)
     print("Image captured and saved by the name {}".format(image_file_name))
