50f322cb712c03ddced2d77f8f0b90ce54ae9a3a
joepol <cpumaster15@gmail.com>
2019-10-20 10:58:24 +0300

    main - commit, just for Tag

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+LOW_CAPTURE_RATE = 600 # seconds
+HIGH_CAPTURE_RATE = 30
+
+# create a directory for each day
+# capture images in two rates (from 05 to 2030)
+# at 21 - pack images to a GIF
+# mail daily GIF
