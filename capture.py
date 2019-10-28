edbd38f92cb9ea19d46de366a3110c36326dce15
joepol <cpumaster15@gmail.com>
2019-10-28 23:50:11 +0200

    Capture - refactor : directory to save image to, logging

---
+++ capture.py
@@ -0,0 +1,28 @@
+import cv2
+import time
+import logging
+
+DEFAULT_DIRECTORY = 'Unknown_directory\\'
+
+logger = logging.getLogger(__name__)
+
+
+camera_port = 0
+camera = cv2.VideoCapture(camera_port, cv2.CAP_DSHOW)
+if not camera.isOpened():
+    logger.critical()
+    raise Exception("Could not open video device")
+
+
+def capture(directory=DEFAULT_DIRECTORY):
+    # camera.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
+    # camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
+    return_value, image = camera.read()
+    image_file_name = directory + "{}.jpeg".format(int(time.time()))
+    cv2.imwrite(image_file_name, image)
+    logger.info("Image captured and saved by the name %s", image_file_name)
+
+
+def shutdown():
+    camera.release()
+    cv2.destroyAllWindows()
