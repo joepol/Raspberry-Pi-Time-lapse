3367da33331a01ae5f9f7c37753edbf6bb447594
joepol <cpumaster15@gmail.com>
2019-10-29 00:28:16 +0200

    logging, checks

--- mail_sender.py
+++ mail_sender.py
@@ -1,22 +1,25 @@
+import os
 import yagmail
 import json
 import sys
 import logging
 
 SUBJECT = "Timelapse GIF"
+MAIL_DATA_FILE_NAME = 'data.txt'
+
+logger = logging.getLogger('__name__')
 
 
 class MailSender:
     def __init__(self):
         # load mailing data
-        with open('data.txt', 'r') as json_file:
+        with open(MAIL_DATA_FILE_NAME, 'r') as json_file:
             data = json.load(json_file)
             self.sender_mail = data['sender_mail']
             self.sender_password = data['sender_password']
             self.receipents_mails = data['receipents']  # a list of email addresses
 
     def send_mail(self, attachment_file_name):
-        # TODO: add current date
         message_content = "This message contain an animated GIF of last 24H time lapse."
 
         yag = yagmail.SMTP(user=self.sender_mail, password=self.sender_password)
@@ -31,13 +34,18 @@ class MailSender:
 
 def main(file_name):
     sender = MailSender()
-    sender.send_mail(attachment_file_name=file_name)
+    # TODO : sanitize input!
+    if os.path.exists(file_name):
+        sender.send_mail(attachment_file_name=file_name)
+    else:
+        logger.error("%s file name doesn't exist, Can't send mail", file_name)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
         main(file_name)
-    # TODO : else - log an error
+    else:
+        logger.error("Must provide a file name when executing mail sender")
 
 main(file_name='images\\timelapse_25_10_2019.gif')
